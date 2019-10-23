3fda9ad85fed807c5b2b5770fdd1fd1a3bb9d059
Jonathan Norman <jnorman2123@gmail.com>
2019-10-23 23:36:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,14 +11,6 @@ class Song
 
   @@all = []
 
-  # def initialize
-  #   self.class.all << self
-  # end
-
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
-
   def self.all
     @@all
   end
@@ -27,7 +19,4 @@ class Song
     @artist = artist
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end
