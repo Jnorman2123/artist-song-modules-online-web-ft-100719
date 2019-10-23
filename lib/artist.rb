2c8bbd741ce5f335499d3f17a4ac9cf4890d9f52
Jonathan Norman <jnorman2123@gmail.com>
2019-10-23 23:37:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,7 +9,7 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
-  @@all = []
+  @@artists = []
 
   def initialize
     super
@@ -17,7 +17,7 @@ class Artist
   end
 
   def self.all
-    @@all
+    @@artists
   end
 
   def add_song(song)
