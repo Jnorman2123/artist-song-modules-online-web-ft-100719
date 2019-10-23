8b2337a5def288ec92592341fbcf2829f1a07f04
Jonathan Norman <jnorman2123@gmail.com>
2019-10-23 23:38:57 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,12 +1,12 @@
 module Findable
 
   @@all = []
-  
+
   def find_by_name(name)
       self.class.all.detect{|a| a.name == name}
   end
 
-  def self.all 
-    @@all 
-  end 
+  def self.all
+    @@all
+  end
 end
